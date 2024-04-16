c47af16dca6c77a145f62db576a2ad931c23293c
ankitdev10 <leoankit35@gmail.com>
2024-04-16 23:45:22 +0545

    codeium and presence

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -9,13 +9,11 @@ opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
-
+opt.smartindent = true
 opt.wrap = false
-
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
-
 
 -- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
@@ -26,3 +24,7 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
+
+-- misc
+
+vim.opt.scrolloff = 10 -- atleast 10 lines will always be visible relative to the cursor
