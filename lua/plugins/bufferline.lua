1fb6c464ce11354a78154552f7335ddf3dc54e66
ankitdev10 <leoankit35@gmail.com>
2024-04-19 09:40:49 +0545

    indicate if buffer has error on the tab

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,22 +1,24 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  event = "VeryLazy",
-  version = "*",
-  keys = {
-    { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
-    { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
-    { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers" },
-    { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
-    { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
-    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
-    { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
-    { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
-    { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
-  },  opts = {
-    options = {
-      separator_style = "slant",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	event = "VeryLazy",
+	version = "*",
+	keys = {
+		{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+		{ "<leader>bd", "<Cmd>bd<CR>", desc = "Delete current buffer" },
+		{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+		{ "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers" },
+		{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+		{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+		{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+		{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+		{ "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+		{ "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+	},
+	opts = {
+		options = {
+			separator_style = "slant",
+			diagnostics = "nvim_lsp",
+		},
+	},
 }
-
